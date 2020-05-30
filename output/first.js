52094b7aa4bb7d772a8a10e7f79b9b2c69bd4446
tanviredu <ornobtanvir.git@gmail.com>
2020-05-30 06:05:44 +0600

    added

--- output/first.js
+++ output/first.js
@@ -2,33 +2,26 @@
 ///<comment> type script starts here</comment>
 /// run the tsc server sart coding
 ///
-
 // defining a class
-class Car{
-    // defining class variable
-    engine:string;
-    
+var Car = /** @class */ (function () {
     // init the class variable 
     // in the constructtor
-    constructor(engine:string){
+    function Car(engine) {
         this.engine = engine;
     }
-    
-    
     // then we make some method
     // and no need to use the function keyword
-    start(){
-        alert("Engine started : "+this.engine);
-    }
-    stop(){
-        alert("Stopping Engine :"+this.engine);
-        
-    }
-}
-
+    Car.prototype.start = function () {
+        alert("Engine started : " + this.engine);
+    };
+    Car.prototype.stop = function () {
+        alert("Stopping Engine :" + this.engine);
+    };
+    return Car;
+}());
 // now we use simple javascript to show this on wimndow onload
-window.onload = function(){
+window.onload = function () {
     var car = new Car("Volvo");
     car.start();
     car.stop();
-}
+};
